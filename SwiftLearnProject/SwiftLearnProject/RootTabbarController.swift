8c20c057bfe65394a240442b13cad4ea35be878a
Allen <shuai.zhang@extantfuture.com>
2016-07-17 23:19:14 +0800

    初始化项目

---
+++ SwiftLearnProject/SwiftLearnProject/RootTabbarController.swift
@@ -0,0 +1,20 @@
+//
+//  RootTabbarController.swift
+//  SwiftLearnProject
+//
+//  Created by Allen on 16/7/17.
+//  Copyright © 2016年 Allen. All rights reserved.
+//
+
+import UIKit
+
+class RootTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}
